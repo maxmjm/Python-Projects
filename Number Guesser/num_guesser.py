32342da88cb12be0dcc38e82336b10ef992f3487
Maxime Morard <maxmjm@icloud.com>
2024-12-26 12:23:39 +0800

    Add lower higher logic

--- Number Guesser/num_guesser.py
+++ Number Guesser/num_guesser.py
@@ -1,25 +1,39 @@
 import random
 
-random_num = random.randrange(-1, 11)
+random_num = random.randrange(0, 11)
+num_guess = 0
+
+print("Welcome to the number guessing game! Guess a number between 0 and 10!")
 
 while True:
+    num_guess += 1
     user_guess = input("Make a guess: ")
     
     if user_guess.isdigit():
         user_guess = int(user_guess)
 
-        if user_guess <= 0:
-            print("Please choose a number greater than 0!")
+        if user_guess < 0:
+            print("Please choose a number greater than or equal to 0!")
+            continue
+
+        if user_guess > 10:
+            print("Please choose a number less than or equal to 10!")
             continue
 
     else:
-        print("Please type a number!")
+        print("Please type a valid number!")
         continue
 
     if user_guess == random_num:
-        print("You got it!")
+        print(f"You got it! The correct number was {random_num}.")
         break
 
     else:
-        print("Wrong try again!")
-        continue
+        if user_guess > random_num:
+            print("Lower!")
+            continue
+        else:
+            print("Higher!")
+            continue
+
+print("It took you", num_guess, "guesses!")
