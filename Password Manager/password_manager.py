592e35c234ae0773afe80ce864d6803d6b2445fe
Maxime Morard <maxmjm@icloud.com>
2024-12-26 17:41:18 +0800

    Define flow of logic for adding and viewing passwords

---
+++ Password Manager/password_manager.py
@@ -0,0 +1,25 @@
+password = input("What is the master password? ")
+
+def add():
+    pass
+
+def view():
+    pass
+
+while True:
+
+    mode = input("Would you like to add a new password or view existing ones (add, view, quit)? ").lower()
+
+    if mode == "quit":
+        print("Exiting program!")
+        break
+
+    if mode == "add":
+        pass
+
+    elif mode == "view":
+        pass
+
+    else:
+        print("Invalid mode!")
+        continue
