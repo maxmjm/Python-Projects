e1c2b9763a20bf3c5dfd48d265a58c3b039147d5
Maxime Morard <maxmjm@icloud.com>
2024-12-26 12:07:58 +0800

    Add logic to guess random number

--- Number Guesser/num_guesser.py
+++ Number Guesser/num_guesser.py
@@ -1,3 +1,25 @@
 import random
 
 random_num = random.randrange(-1, 11)
+
+while True:
+    user_guess = input("Make a guess: ")
+    
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+
+        if user_guess <= 0:
+            print("Please choose a number greater than 0!")
+            continue
+
+    else:
+        print("Please type a number!")
+        continue
+
+    if user_guess == random_num:
+        print("You got it!")
+        break
+
+    else:
+        print("Wrong try again!")
+        continue
