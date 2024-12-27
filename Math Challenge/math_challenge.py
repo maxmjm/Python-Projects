79f511696acc837682e01110015a9434e4188ebd
Maxime Morard <maxmjm@icloud.com>
2024-12-27 12:50:40 +0800

    Calculate time taken to complete problems

--- Math Challenge/math_challenge.py
+++ Math Challenge/math_challenge.py
@@ -1,4 +1,5 @@
 import random 
+import time
 
 OPERATORS = ["+", "-", "*"]
 MIN_OPERAND = 4
@@ -16,10 +17,24 @@ def generate_problem():
 
     return expression, answer
 
+wrong_guess = 0
+
+input("Press enter to start!")
+print("---------------------")
+
+start_time = time.time()
+
 for i in range(TOTAL_PROBLEMS):
     expression, answer = generate_problem()
     while True: 
         guess = input("Problem #" + str(i + 1) + ": " + expression + " = ") 
         if guess == str(answer):
             break
+        wrong_guess += 1
+
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
+
+print("---------------------")
+print("You finished in", total_time, "seconds with an accuracy of")
         
