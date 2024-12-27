bd3c5f6bce65784cc3ccb690ee9f6f9fedab1557
Maxime Morard <maxmjm@icloud.com>
2024-12-27 12:40:24 +0800

    Create loop for each math problem

--- Math Challenge/math_challenge.py
+++ Math Challenge/math_challenge.py
@@ -10,8 +10,16 @@ def generate_problem():
     right_operand = random.randint(MIN_OPERAND, MAX_OPERAND)
     operator = random.choice(OPERATORS)
 
-    expression = str(left_operand) + " " + operator + str(right_operand)
+    expression = str(left_operand) + " " + operator + " " + str(right_operand)
 
     answer = eval(expression)
 
     return expression, answer
+
+for i in range(TOTAL_PROBLEMS):
+    expression, answer = generate_problem()
+    while True: 
+        guess = input("Problem #" + str(i + 1) + ": " + expression + " = ") 
+        if guess == str(answer):
+            break
+        
