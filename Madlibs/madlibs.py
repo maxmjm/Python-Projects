3d1b5ad27d2d45350e7fe24025dc6e5d20bc2505
Maxime Morard <maxmjm@icloud.com>
2024-12-27 11:43:26 +0800

    Logic for parsing words in file

--- Madlibs/madlibs.py
+++ Madlibs/madlibs.py
@@ -6,3 +6,12 @@ start_of_word = -1
 
 target_start = "<"
 target_end = ">"
+
+for i, char in enumerate(story):
+    if char == target_start:
+        start_of_word = i
+    
+    if char == target_end and start_of_word != -1:
+        word = story[start_of_word: i + 1]
+        words_in_file.add(word)
+        start_of_word = -1
