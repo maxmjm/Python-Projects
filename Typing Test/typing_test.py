0a90dcb3c57dc71127d75f5e0fdc5440c603ee1c
Maxime Morard <maxmjm@icloud.com>
2024-12-27 18:19:25 +0800

    Logic to start typing speed test

--- Typing Test/typing_test.py
+++ Typing Test/typing_test.py
@@ -1,15 +1,20 @@
 import curses
 from curses import wrapper
 
+def start_screen(standard_output_screen):
+    standard_output_screen.clear()
+    standard_output_screen.addstr("Welcome to the Typing Speed Test!")
+    standard_output_screen.addstr("\nPress any key to begin!")
+    standard_output_screen.refresh()
+    key = standard_output_screen.getkey()
+
+
 def main(standard_output_screen):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-    standard_output_screen.clear()
-    standard_output_screen.addstr("Hello world!", curses.color_pair(1))
-    standard_output_screen.refresh()
-    standard_output_screen.getkey()
+    start_screen(standard_output_screen)
 
 wrapper(main)
     
