3d1933bcf2affb90be2444c5bd31b11adc294900
Maxime Morard <maxmjm@icloud.com>
2024-12-27 11:03:03 +0800

    Validate number of players

--- Dice Game/dice_game.py
+++ Dice Game/dice_game.py
@@ -6,3 +6,17 @@ def roll_dice():
     roll_dice = random.randint(min_val, max_val)
 
     return roll_dice
+
+while True:
+    num_players = input("Enter number of players (1-4): ")
+    if num_players.isdigit():
+        num_players = int(num_players)
+        if 2 <= num_players <= 4:
+            break
+        else:
+            print("Invalid number of players!")
+    else:
+        print("Invalid input try again!")
+
+max_score = 60
+player_scores = [0 for _ in range(num_players)]
