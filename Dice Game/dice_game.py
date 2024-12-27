8051de169333b18495093fbbb0520324d30a5085
Maxime Morard <maxmjm@icloud.com>
2024-12-27 11:09:19 +0800

    Scoring logic for all players

--- Dice Game/dice_game.py
+++ Dice Game/dice_game.py
@@ -4,7 +4,6 @@ def roll_dice():
     min_val = 1
     max_val = 6
     roll_dice = random.randint(min_val, max_val)
-
     return roll_dice
 
 while True:
@@ -26,4 +25,19 @@ while max(player_scores) < max_score:
         print("\nPlayer number", player_index + 1, "turn has just started!")
         print("Your total score is", player_scores[player_index], "\n")
         current_score = 0
-        
+
+        while True:
+            roll = input("Would you like to roll (y)? ")
+            if roll.lower() != "y":
+                break
+            value = roll_dice()
+            if value == 1:
+                print("You rolled a 1! Turn over!")
+                current_score = 0
+                break
+            else: 
+                current_score += value
+                print("You rolled a", value)
+            print("Your score is", current_score)
+        player_scores[player_index] += current_score
+        print("Your total score is", player_scores[player_index])
