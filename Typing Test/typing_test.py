9514190633fe0a305b9d45ff63d2b095eecd5da3
Maxime Morard <maxmjm@icloud.com>
2024-12-27 18:05:52 +0800

    Initialise curses text colours

--- Typing Test/typing_test.py
+++ Typing Test/typing_test.py
@@ -2,8 +2,12 @@ import curses
 from curses import wrapper
 
 def main(standard_output_screen):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
     standard_output_screen.clear()
-    standard_output_screen.addstr("Hello world!")
+    standard_output_screen.addstr("Hello world!", curses.color_pair(1))
     standard_output_screen.refresh()
     standard_output_screen.getkey()
 
