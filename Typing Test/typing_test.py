a8e5424f69cbdd93d6ab99765238471f8b153433
Maxime Morard <maxmjm@icloud.com>
2024-12-30 14:27:45 +0800

    Renamed variable to stdscr

--- Typing Test/typing_test.py
+++ Typing Test/typing_test.py
@@ -2,16 +2,16 @@ import curses
 from curses import wrapper
 import time
 
-def start_screen(standard_output_screen):
-    standard_output_screen.clear()
-    standard_output_screen.addstr("Welcome to the Typing Speed Test!")
-    standard_output_screen.addstr("\nPress any key to begin!")
-    standard_output_screen.refresh()
-    key = standard_output_screen.getkey()
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Welcome to the Typing Speed Test!")
+    stdscr.addstr("\nPress any key to begin!")
+    stdscr.refresh()
+    key = stdscr.getkey()
 
-def display_text(standard_output_screen, target, current, wpm = 0):
-    standard_output_screen.addstr(target)
-    standard_output_screen.addstr(1, 0, f"WPM: {wpm}")
+def display_text(stdscr, target, current, wpm = 0):
+    stdscr.addstr(target)
+    stdscr.addstr(1, 0, f"WPM: {wpm}")
 
     for i, char in enumerate(current):
         correct_char = target[i]
@@ -19,9 +19,9 @@ def display_text(standard_output_screen, target, current, wpm = 0):
         if char != correct_char:
             colour = curses.color_pair(2)
 
-        standard_output_screen.addstr(0, i, char, colour)
+        stdscr.addstr(0, i, char, colour)
 
-def wpm_typing_test(standard_output_screen):
+def wpm_typing_test(stdscr):
     target_text = "Hello world this is some text for the app!"
     current_text = []
     wpm = 0
@@ -30,18 +30,18 @@ def wpm_typing_test(standard_output_screen):
     while True:
         time_elapsed = max(time.time() - start_time, 1)
         wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
-        standard_output_screen.nodelay(True)
+        stdscr.nodelay(True)
 
-        standard_output_screen.clear()
-        display_text(standard_output_screen, target_text, current_text, wpm)
-        standard_output_screen.refresh()
+        stdscr.clear()
+        display_text(stdscr, target_text, current_text, wpm)
+        stdscr.refresh()
 
         if "".join(current_text) == target_text:
-            standard_output_screen.nodelay(False)
+            stdscr.nodelay(False)
             break
 
         try:
-            key = standard_output_screen.getkey()
+            key = stdscr.getkey()
         except:
             continue
 
@@ -54,17 +54,17 @@ def wpm_typing_test(standard_output_screen):
         elif len(current_text) < len(target_text):
             current_text.append(key)
 
-def main(standard_output_screen):
+def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-    start_screen(standard_output_screen)
+    start_screen(stdscr)
 
     while True:
-        wpm_typing_test(standard_output_screen)
-        standard_output_screen.addstr(2, 0, "You completed the text! Press any key to continue...")
-        key = standard_output_screen.getkey()
+        wpm_typing_test(stdscr)
+        stdscr.addstr(2, 0, "You completed the text! Press any key to continue...")
+        key = stdscr.getkey()
 
         if ord(key) == 27:
             break
