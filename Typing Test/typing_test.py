b8905a70e12f2c034592fb39e9bcb08988bc1297
Maxime Morard <maxmjm@icloud.com>
2024-12-30 11:48:46 +0800

    Fixed typing overlay

--- Typing Test/typing_test.py
+++ Typing Test/typing_test.py
@@ -8,28 +8,36 @@ def start_screen(standard_output_screen):
     standard_output_screen.refresh()
     key = standard_output_screen.getkey()
 
+def display_text(standard_output_screen, target, current, wpm = 0):
+    standard_output_screen.addstr(target)
+
+    for i, char in enumerate(current):
+        correct_char = target[i]
+        colour = curses.color_pair(1)
+        if char != correct_char:
+            colour = curses.color_pair(2)
+
+        standard_output_screen.addstr(0, i, char, colour)
+
 def wpm_typing_test(standard_output_screen):
     target_text = "Hello world this is some text for the app!"
     current_text = []
 
     while True:
         standard_output_screen.clear()
-        standard_output_screen.addstr(target_text)
-
-        for char in current_text:
-            standard_output_screen.addstr(char, curses.color_pair(1))
-
+        display_text(standard_output_screen, target_text, current_text)
         standard_output_screen.refresh()
 
         key = standard_output_screen.getkey()
 
         if ord(key) == 27: # Escape key
             break
+
         if key in ("KEY_BACKSPACE", "\b", "\x7f"):
             if len(current_text) > 0:
                 current_text.pop()
-            else:
-                current_text.append(key)
+        elif len(current_text) < len(target_text):
+            current_text.append(key)
 
 def main(standard_output_screen):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
