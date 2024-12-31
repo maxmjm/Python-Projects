a5d065b2c35c00e691f20d19630d01450381e58c
Maxime Morard <maxmjm@icloud.com>
2024-12-31 13:46:49 +0800

    Clear terminal and do not print on new line

--- Alarm and Timer/alarm_timer.py
+++ Alarm and Timer/alarm_timer.py
@@ -1,9 +1,13 @@
 from playsound import playsound
 import time
 
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\033[H"
+
 def alarm(seconds):
     time_elapsed = 0
 
+    print(CLEAR)
     while time_elapsed < seconds:
         time.sleep(1)
         time_elapsed += 1
@@ -12,6 +16,6 @@ def alarm(seconds):
         minutes_left = time_left // 60
         seconds_left = time_left % 60
 
-        print(f"{minutes_left:02d}:{seconds_left:02d}")
+        print(f"{CLEAR_AND_RETURN}{minutes_left:02d}:{seconds_left:02d}")
 
 alarm(10)
