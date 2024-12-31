210d57dc5919862514cd5d6f7e6c0f84b534fee9
Maxime Morard <maxmjm@icloud.com>
2024-12-31 14:20:24 +0800

    Renamed variables

--- Password Generator/password_generator.py
+++ Password Generator/password_generator.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-def generate_password(min_length, numbers = True, special_char = True):
+def generate_password(min_length, numbers = True, special_characters = True):
     letters = string.ascii_letters
     digits = string.digits
     special = string.punctuation
@@ -9,34 +9,34 @@ def generate_password(min_length, numbers = True, special_char = True):
     characters = letters
     if numbers:
         characters += digits
-    if special_char:
+    if special_characters:
         characters += special
 
-    pwd = ""
+    password = ""
     meets_criteria = False
     has_number = False
     has_special = False
 
-    while not meets_criteria or len(pwd) < min_length:
-        new_char = random.choice(characters)
-        pwd += new_char
+    while not meets_criteria or len(password) < min_length:
+        new_character = random.choice(characters)
+        password += new_character
 
-        if new_char in digits:
+        if new_character in digits:
             has_number = True
-        elif new_char in special:
+        elif new_character in special:
             has_special = True
 
-        meets_criteria = (len(pwd) >= min_length)
+        meets_criteria = (len(password) >= min_length)
         if numbers:
             meets_criteria = has_number
-        if special_char:
+        if special_characters:
             meets_criteria =  meets_criteria and has_special
 
-    return pwd
+    return password
 
 min_length = int(input("Enter minimum length of password: "))
 has_number = input("Do you want the password to have numbers (y/n)? ").lower() == "y"
 has_special = input("Do you want the password to have special characters (y/n)? ").lower() == "y"
 
-pwd = generate_password(min_length, has_number, has_special)
-print("The generated password is:", pwd)
+password = generate_password(min_length, has_number, has_special)
+print("The generated password is:", password)
